1705b632275ee94b7e1c4c24b1ac5316f65dc4c1
Bruno Barbosa <brunobarbosa90@hotmail.com>
2024-08-22 14:08:15 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 23 + 34 + 12;
+            int b = 1000 - 34 - 67 - 23;
+            int c = 123 * 23;
+            float d = 123.56f * 14;
+            float e = 1223 * 12.32f;
+            float f = 12.45f * 32.4f;
+            float g = 12.34f + 23.34f;
+            float h = 324.5678f - 348.987f;
+            float i = 86f / 12;
+            int j = 10 / 2;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.WriteLine(g);
+            Console.WriteLine(h);
+            Console.WriteLine(i);
+            Console.WriteLine(j);
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
